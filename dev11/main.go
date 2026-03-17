20cc14743a154cbe8517f68c0fdb94a1b83eae33
TolkinSL <tolkinsl@yandex.ru>
2026-03-17 12:37:50 +0300

    dev11a context

--- dev11/main.go
+++ dev11/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
-func worker(worker int, ch chan <- string) {
-	for range time.Tick(time.Second) {
-		ch <- fmt.Sprintf("Hi worker : %d", worker)
+func worker(ctx context.Context, wg *sync.WaitGroup, worker int, ch chan <- string) {
+	defer wg.Done()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <- ticker.C:
+			ch <- fmt.Sprintf("Hi worker : %d", worker)
+		case <- ctx.Done():
+			return
+		}
+		
 	}
 }
 
 func main() {
 	tasks := make(chan string, 10)
 
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
 	for i:= range 10 {
-		go worker(i, tasks)
+		wg.Add(1)
+		go worker(ctx, &wg, i, tasks)
 	}
+
+	go func() {
+		wg.Wait()
+		close(tasks)
+	}()
 
 	for str := range tasks {
 		fmt.Println(str)
 	}
+
 }
